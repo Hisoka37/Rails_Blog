4d7a58a31d6190125ca9ec4ab5b63d7fc6fa880b
Walid Kabboussa <walidkabou@gmail.com>
2023-06-16 11:50:42 +0100

    Fix Linters

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -43,28 +43,28 @@ RSpec.describe Post, type: :model do
       subject.update_user_posts_counter
       expect(subject.author.posts_counter).to eq(2)
     end
-end
+  end
 
-describe '#recent_comments' do
-        let(:user) { User.create(name: 'Walid', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer From Morocco .', posts_counter: 0) }
-        let!(:post) { user.posts.create(title: 'Post 1', text: 'my post 1', comments_counter: 0, likes_counters: 0) }
-        let!(:comment1) { post.comments.create(text: 'Comment 1', author_id: user.id) }
-        let!(:comment2) { post.comments.create(text: 'Comment 2', author_id: user.id) }
-        let!(:comment3) { post.comments.create(text: 'Comment 3', author_id: user.id) }
-        let!(:comment4) { post.comments.create(text: 'Comment 4', author_id: user.id) }
-        let!(:comment5) { post.comments.create(text: 'Comment 5', author_id: user.id) }
-        let!(:comment6) { post.comments.create(text: 'Comment 6', author_id: user.id) }
-    
-        it 'returns the most recent 3 comments' do
-          recent_comments = post.latest_commments
-          expect(recent_comments.length).to eq(5)
-          expect(recent_comments).to contain_exactly(comment6, comment5, comment4, comment3, comment2)
-        end
-    
-        it 'returns an empty array if the post has no comments' do
-          post.comments.destroy_all
-          recent_comments = post.latest_commments
-          expect(recent_comments).to be_empty
-        end
-      end
+  describe '#recent_comments' do
+    let(:user) { User.create(name: 'Walid', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer From Morocco .', posts_counter: 0) }
+    let!(:post) { user.posts.create(title: 'Post 1', text: 'my post 1', comments_counter: 0, likes_counters: 0) }
+    let!(:comment1) { post.comments.create(text: 'Comment 1', author_id: user.id) }
+    let!(:comment2) { post.comments.create(text: 'Comment 2', author_id: user.id) }
+    let!(:comment3) { post.comments.create(text: 'Comment 3', author_id: user.id) }
+    let!(:comment4) { post.comments.create(text: 'Comment 4', author_id: user.id) }
+    let!(:comment5) { post.comments.create(text: 'Comment 5', author_id: user.id) }
+    let!(:comment6) { post.comments.create(text: 'Comment 6', author_id: user.id) }
+
+    it 'returns the most recent 3 comments' do
+      recent_comments = post.latest_commments
+      expect(recent_comments.length).to eq(5)
+      expect(recent_comments).to contain_exactly(comment6, comment5, comment4, comment3, comment2)
+    end
+
+    it 'returns an empty array if the post has no comments' do
+      post.comments.destroy_all
+      recent_comments = post.latest_commments
+      expect(recent_comments).to be_empty
+    end
+  end
 end
