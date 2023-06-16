dbe2206bcddb584ffaaadb2474f61c6b7ce0319d
Walid Kabboussa <walidkabou@gmail.com>
2023-06-16 11:47:08 +0100

    Run Test for comment and Like Modul

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Post, type: :model do
 end
 
 describe '#recent_comments' do
-        let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer From Morocco .', posts_counter: 0) }
+        let(:user) { User.create(name: 'Walid', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer From Morocco .', posts_counter: 0) }
         let!(:post) { user.posts.create(title: 'Post 1', text: 'my post 1', comments_counter: 0, likes_counters: 0) }
         let!(:comment1) { post.comments.create(text: 'Comment 1', author_id: user.id) }
         let!(:comment2) { post.comments.create(text: 'Comment 2', author_id: user.id) }
