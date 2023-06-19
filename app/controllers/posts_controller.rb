c4c9fd0adae391cf4ba63e8c58f1091be5168a9c
Walid Kabboussa <walidkabou@gmail.com>
2023-06-19 19:27:40 +0100

    Setup controllers and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    def index 
+        render plain: "Here is a list of posts"
+    end
+
+    def show
+        render plain: "Here is a single Post"
+    end
+end
+
